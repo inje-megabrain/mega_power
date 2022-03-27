d4a6bc76fdce5aa3a7759afa33c250d564fdbabf
MoonSangWon <kuim75@naver.com>
2022-03-27 20:07:35 +0900

    #8 폴더 수정

--- Python/111720.py
+++
@@ -1,5 +0,0 @@
-#숫자 자릿수 덧셈
-count = int(input())
-num = input()
-s = list(map(int,str(num)))
-print(sum(s))
--- Python/2675.py
+++
@@ -1,7 +0,0 @@
-langs = int(input())
-for _ in range(langs):
-    c = list(input().split(' '))
-    voca_list = list(c[1])
-    for i in voca_list:
-        print(int(c[0])*i,end='')
-    print() 
