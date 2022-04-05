4bb9f4f32d9853dc54b254d06ae9ca14681a2eb7
choijinseo <chch4lee1@naver.com>
2022-04-05 10:28:40 +0900

    #22 04M 05D 마무리

--- choijinseo/백준/10809번.py
+++ choijinseo/백준/10809번.py
@@ -1,7 +1,10 @@
-S = list(map(str, input()))
+S = list(map(str, input())) # baekjoon
 alpha = ['a','b','c','d','e','f','g','h','i','j','k','l'
 ,'m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 print(S)
-for i in range(26):
-    if S[i] == alpha[i]:
-        print(alpha.index(alpha[i]),end="")
+for i in range(len(S)):
+    for j in range(27):
+        if S[i] == alpha[j]:
+            print(alpha.index(j),end="")
+        else:
+            print("-1",end="")
