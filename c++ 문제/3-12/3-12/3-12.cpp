95981585616f46d3cf2ae9ddb6be70ef4d1c91f7
mjh000526 <mjh000526@gmail.com>
2022-05-01 20:31:24 +0900

    #46 3주차 커리큘럼

--- c++ 문제/3-12/3-12/3-12.cpp
+++ c++ 문제/3-12/3-12/3-12.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
-#include "헤더.h"
+#include <cstring>
 using namespace std;
 
+class book {
+	char* title;
+	int price;
+public:
+	book(string str, int price) {
+		this->price = price;
+		int len = str.length();
+		title = new char[len];
+		for (int i = 0; i < len; i++) {
+			title[i] = str[i];
+		}
+	}
+	book(book& b) {
+		price = b.price;
+		int len = strlen(b.title);
+		title = new char[len+1];
+ 		strcpy_s(title, b.title);
+	}
+	void set(string str, int price) {
+		this->price = price;
+		int len = str.length();
+		title = new char[len];
+		for (int i = 0; i < len; i++) {
+			title[i] = str[i];
+		}
+	}
+	void show() { cout << title << ' ' << price << "원" << endl; }
+};
+
 int main() {
-	Ram ram;
-	ram.write(100, 20);
-	ram.write(101, 30);
-	char res = ram.read(100) + ram.read(101);
-	ram.write(102, res);
-	cout << "102 번지의 값 = " << (int)ram.read(102) << endl;
+	book cpp("명품c++", 10000);
+	book java = cpp;
+	java.set("명품자바", 12000);
+	cpp.show();
+	java.show();
 };
