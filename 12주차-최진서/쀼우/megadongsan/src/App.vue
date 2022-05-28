94ec3b75aa3ca07ee841e193ea5320e9407c7110
Dark-jin <chch4lee1@naver.com>
2022-05-28 16:53:36 +0900

    #102 Vue 맛보기

---
+++ 12주차-최진서/쀼우/megadongsan/src/App.vue
@@ -0,0 +1,94 @@
+<template>
+
+  <div class="black-bg" v-if="모달창열렸니==true">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+      <button @click="모달창열렸니=false">닫기</button>
+    </div>
+  </div>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a>
+  </div>
+
+  <div v-for="(룸,i) in 6" :key="룸">
+    <img :src="원룸들[i].image" class="room-img">
+    <h4 @click="모달창열렸니=true">{{원룸들[i].title}}</h4>
+    <p>{{원룸들[i].price}}</p>
+  </div>
+  <!-- <button v-on:click="신고수[0]++">허위매물신고</button> <span>신고수 : {{신고수[0]}}</span> -->
+  <!-- <div v-for="(룸,i) in 3" :key="룸">
+    <h4>{{products[i]}}</h4>
+    <p>{{prices[i]}}</p>
+  </div> -->
+</template>
+
+<script>
+
+import data from './data.js';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0,0,0],
+      메뉴들 : ['Home','Shop','About'],
+      products : ['역삼동 원룸','천호동 원룸','마포구 원룸'],
+      prices : ['30만원','60만원','100만원'],
+    }
+  },
+  methods : {
+    increase() {
+      this.신고수 +=1;
+    }
+  },
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+body {
+  margin :0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height: 100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.menu {
+  background:darkslateblue;
+  padding :15px;
+  border-radius:5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
+</style>
---
+++ 12주차-최진서/쀼우/megadongsan/src/data.js
@@ -0,0 +1,42 @@
+export default [{
+    id : 0,
+    title: "Sinrim station 30 meters away",
+    image: "https://codingapple1.github.io/vue/room0.jpg",
+    content: "18년 신축공사한 남향 원룸 ☀️, 공기청정기 제공",
+    price: 340000
+    },
+    {
+    id : 1,
+    title: "Changdong Aurora Bedroom(Queen-size)",
+    image: "https://codingapple1.github.io/vue/room1.jpg",
+    content: "침실만 따로 있는 공용 셰어하우스입니다. 최대 2인 가능",
+    price: 450000
+    },
+    {
+    id : 2,
+    title: "Geumsan Apartment Flat",
+    image: "https://codingapple1.github.io/vue/room2.jpg",
+    content: "금산오거리 역세권 아파트입니다. 애완동물 불가능 ?",
+    price: 780000
+    },
+    {
+    id : 3,
+    title: "Double styled beds Studio Apt",
+    image: "https://codingapple1.github.io/vue/room3.jpg",
+    content: "무암동인근 2인용 원룸입니다. 전세 전환가능",
+    price: 550000
+    },
+    {
+    id : 4,
+    title: "MyeongIl Apartment flat",
+    image: "https://codingapple1.github.io/vue/room4.jpg",
+    content: "탄천동 아파트 월세, 남향, 역 5분거리, 허위매물아님",
+    price: 680000
+    },
+    {
+    id : 5,
+    title: "Banziha One Room",
+    image: "https://codingapple1.github.io/vue/room5.jpg",
+    content: "반지하 원룸입니다. 비올 때 물가끔 새는거 빼면 좋아요",
+    price: 370000
+  }];
